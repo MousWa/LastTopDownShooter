ccf99a3da5cd49a25c6b93a5230c277d97049208
wajih96 <wajih.helali@tu-dortmund.de>
2021-05-09 04:04:26 +0200

    Bullet Animation

--- New Unity Project/Assets/Scrites/Shooting.cs
+++ New Unity Project/Assets/Scrites/Shooting.cs
@@ -6,7 +6,7 @@ public class Shooting : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject bulletPerfab;
-    public float buzlletSpeed = 20f;
+    public float buzlletSpeed = 15f;
 
     // Update is called once per frame
     void Update()
