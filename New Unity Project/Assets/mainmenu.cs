8289a12a36816a222e30877570beb56ae8d12d87
MousWa <57647317+MousWa@users.noreply.github.com>
2021-05-11 00:31:45 +0200

    s

--- New Unity Project/Assets/mainmenu.cs
+++ New Unity Project/Assets/mainmenu.cs
@@ -9,4 +9,9 @@ public class mainmenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
 }
