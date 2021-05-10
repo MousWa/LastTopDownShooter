eb8518b86e602f4c03ee7245272c35fe94dd57b3
MousWa <57647317+MousWa@users.noreply.github.com>
2021-05-10 02:34:36 +0200

    sa

--- New Unity Project/Assets/Scrites/Enemey.cs
+++ New Unity Project/Assets/Scrites/Enemey.cs
@@ -1,35 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey.Utils;
 
 public class Enemey : MonoBehaviour
 {
-    public Transform player;
-    public float moveSpeed = 5f;
-    private Rigidbody2D rb;
-    private Vector2 movement;
-
+    private EnemyPathfindingMovement pathfindingMovment;
+    private Vector3 startingPostiton;
+    private Vector3 roamPostition;
+   
+    private void Awake()
+    {
+        pathfindingMovment = GetComponent<EnemyPathfindingMovement>();
+    }
     // Start is called before the first frame update
     void Start()
     {
-        rb = this.GetComponent<Rigidbody2D>();
+        startingPostiton = transform.position;
+        roamPostition = GetRoamingPosition();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = player.position - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        rb.rotation = angle;
-        direction.Normalize();
-        movement = direction;
+        pathfindingMovment.MoveToTimer(roamPostition);
+        float reachedPositionDistance = 10f;
+        if(Vector3.Distance(transform.position, roamPostition)< reachedPositionDistance)
+        {
+            roamPostition = GetRoamingPosition();
+        }
     }
-    private void FixedUpdate()
+    Vector3 GetRoamingPosition()
     {
-        moveCharacter(movement);
-    }
-    void moveCharacter(Vector2 direction)
-    {
-        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+        return startingPostiton + UtilsClass.GetRandomDir() * Random.Range(10f, 70f);
     }
 }
