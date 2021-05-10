dd20424db8ee8343ec6320fbc550af7a23b4bb89
MousWa <57647317+MousWa@users.noreply.github.com>
2021-05-10 18:32:28 +0200

    firesound

--- New Unity Project/Assets/Scrites/Shooting.cs
+++ New Unity Project/Assets/Scrites/Shooting.cs
@@ -7,13 +7,15 @@ public class Shooting : MonoBehaviour
     public Transform firePoint;
     public GameObject bulletPerfab;
     public float buzlletSpeed ;
+    public static AudioClip fireSound;
+    static AudioSource audioSrc;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Fire1")) {
             Shoot();
-            FireSound.PlaySound("fire");
+            FireSound.PlaySound("Fire");
         }
     }
     void Shoot() {
@@ -21,4 +23,14 @@ public class Shooting : MonoBehaviour
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(firePoint.up * buzlletSpeed, ForceMode2D.Impulse);
     }
+    /*
+    public void PlaySound(string clip)
+    {
+        switch (clip)
+        {
+            case "Fire":
+                audioSrc.PlayOneShot(fireSound);
+                break;
+        }
+    }*/
 }
