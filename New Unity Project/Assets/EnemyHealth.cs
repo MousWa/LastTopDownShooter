270f51d2c5f9db81b1ab2172b41c36c5f2be14e7
MousWa <57647317+MousWa@users.noreply.github.com>
2021-05-11 07:53:15 +0200

    sa

--- New Unity Project/Assets/EnemyHealth.cs
+++ New Unity Project/Assets/EnemyHealth.cs
@@ -6,7 +6,7 @@ public class EnemyHealth : MonoBehaviour
 {
     public int max = 100;
     public int current;
-   
+    public GameObject Damagepopup;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,23 +16,31 @@ public class EnemyHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
         if (Input.GetKeyDown(KeyCode.N))
         {
             TakeDamage(20);
         }
-        if (current == 0) {
+        if (current <= 0  ) {
             Destroy(gameObject);
         }
     }
-   
-    
+
+    void ShowFloatingText()
+    {
+        Damagepopup = Instantiate(Damagepopup, transform.position, Quaternion.identity);
+        Damagepopup.GetComponent<TextMesh>().text = current.ToString() ;
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         print("kjasdbjkasbdas");
         if (collision.collider.CompareTag("playerBulett"))
         {
             TakeDamage(20);
+            if (Damagepopup)
+            {
+                ShowFloatingText();
+            }
         }
     }
     void TakeDamage(int damage)
